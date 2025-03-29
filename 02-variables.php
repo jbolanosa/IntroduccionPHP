cf75c623ba8286a97e7eb6a116da17268aede1ca
José Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-03-28 20:05:44 -0600

    variables

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,20 @@
 <?php include 'includes/header.php';
 
+$nombre = 'Jose';
 
 
+echo $nombre;
+var_dump($nombre);
+
+
+define('constante', "Este es el valor de la constante");
+
+echo constante;
+
+
+//no es tan comun la siguiente forma
+const constante2 = "Hola3";
+
+echo constante2;
 
 include 'includes/footer.php';
