354c74c5518a096cb09e2bdafd533868add2deaa
José Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-03-28 20:18:36 -0600

    operadores

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,6 +1,17 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 10;
 
+echo $numero1 + $numero2;
+echo "<br>";
+echo $numero1 - $numero2;
+echo "<br>";
+echo $numero1 * $numero2;
+echo "<br>";
+echo $numero1 / $numero2;
+echo "<br>";
+echo 2 ** 3;
 
 
 include 'includes/footer.php';
