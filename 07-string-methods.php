e54b060335528ec41f8b37cd782558256b238d6d
José Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-03-29 10:24:11 -0600

    metodos string

--- 07-string-methods.php
+++ 07-string-methods.php
@@ -1,6 +1,29 @@
 <?php include 'includes/header.php';
 
 
+$nombreCliente = "Juan Pablo";
 
+//tamaño de un string
+echo strlen($nombreCliente);
+
+$texto = trim($nombreCliente);
+echo strlen($texto);
+
+
+echo strtoupper($nombreCliente);
+
+echo strtolower($nombreCliente);
+
+echo str_replace('Juan', 'J', $nombreCliente);
+
+
+echo strpos($nombreCliente, 'Pablo');
+
+$tipoCliente = "Premium";
+
+echo "El cliente " . $nombreCliente . " es " . $tipoCliente;
+
+//esta deprecado
+//echo "El cliente {$nombreCliente} es ${tipoCliente}";
 
 include 'includes/footer.php';
