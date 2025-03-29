3c9e73a4ec4364e8171ba898ea7025062fced57d
José Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-03-28 20:15:04 -0600

    tipos de variables

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,24 @@
 <?php include 'includes/header.php';
 
+//booleano
+$logueado = true;
+var_dump($logueado);
 
+//enteros
+$numero = 200;
+var_dump($numero);
+
+//float
+$float = 200.5;
+var_dump($float);
+
+//strings
+$nombre = "Juan";
+var_dump($nombre);
+
+//arrays
+$array = [];
+var_dump($array);
 
 
 include 'includes/footer.php';
