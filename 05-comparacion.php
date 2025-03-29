c343167d967e0d0417909cc996d5dbc3114788b2
José Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-03-29 09:09:38 -0600

    comparacion

--- 05-comparacion.php
+++ 05-comparacion.php
@@ -2,6 +2,37 @@
 
 
 
+$numero1 = 20;
+$numero2 = 30;
+$numero3 = 30;
+$numero4 = "30";
 
+var_dump($numero1 > $numero2);
+echo "<br/>";
+
+var_dump($numero1 < $numero2);
+echo "<br/>";
+
+var_dump($numero1 >= $numero2);
+echo "<br/>";
+
+var_dump($numero1 <= $numero2);
+echo "<br/>";
+
+var_dump($numero2 == $numero3);
+echo "<br/>";
+
+var_dump($numero2 == $numero4);
+echo "<br/>";
+
+var_dump($numero2 === $numero4);
+echo "<br/>";
+
+var_dump($numero1 <=> $numero2);
+echo "<br/>";
+
+//si es mayor el de la izquierda da -1, si son iguales 0, si es mayor el otro 1
+var_dump($numero2 <=> $numero2);
+echo "<br/>";
 
 include 'includes/footer.php';
