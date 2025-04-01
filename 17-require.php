1ffed1544469927425893a09f7e8254ac4ab7454
José Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-04-01 10:56:23 -0600

    require

--- 17-require.php
+++ 17-require.php
@@ -1,6 +1,13 @@
 <?php include 'includes/header.php';
 
+require('funciones.php');
 
+
+iniciarApp();
+
+//usar require para funciones criticas como la conexion de una base de datos
+//usar include para importar templates
+//require_ones solo incluye una vez, revisa el codigo
 
 
 
