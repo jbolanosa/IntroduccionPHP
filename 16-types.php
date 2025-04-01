7d3782c7d916879037fbcf39bc684828dda59e02
José Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-04-01 10:19:57 -0600

    types

--- 16-types.php
+++ 16-types.php
@@ -1,6 +1,28 @@
 <?php include 'includes/header.php';
 
 
+//podemos definir el tipo devuelto
+function usuarioAutenticado(bool $autenticado) : ?string {
+    if($autenticado){
+        return "El usuario esta autenticado";
+    } else {
+        return null;
+    }
+}
+
+function usuarioAutenticado2(bool $autenticado) : string|int {
+    if($autenticado){
+        return "El usuario esta autenticado";
+    } else {
+        return 20;
+    }
+}
+
+
+
+$usuario = usuarioAutenticado(true);
+
+echo $usuario;
 
 
 
