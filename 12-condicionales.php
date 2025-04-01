291fefc02278b74c380d237e3a08bfd9ca807d25
José Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-03-31 20:12:59 -0600

    condicionales

--- 12-condicionales.php
+++ 12-condicionales.php
@@ -1,7 +1,58 @@
 <?php include 'includes/header.php';
 
 
+$autenticado = true;
+$admin = false;
+
+if($autenticado || $admin) {  //o usando &&
+    echo "Usuario autenticado";
+} else {
+    echo "Usuario no autenticado";
+}
 
 
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'Premium'
+    ]
+];
+
+
+if (empty($cliente)) {
+    echo 'El arreglo de cliente no esta vacio';
+
+    if(!$cliente['saldo'] > 0){
+        echo 'Sado disponible';
+    } else {
+        echo 'No hay saldo';
+    }
+} else {
+
+}
+
+
+if($cliente['saldo'] > 0){
+    echo "El cliente tiene saldo";
+} else if($cliente['informacion']['tipo'] === 'Premium'){
+    echo 'El cliente es premium';
+} else {
+    echo 'No hay cliente definido o no tiene saldo';
+}
+
+
+$tecnologia = 'PHP';
+
+
+switch ($tecnologia) {
+    case 'PHP':
+        echo 'PHP, un excelente lenguaje';
+        break;
+    
+    default:
+        echo 'Un lenguaje que no se cual es';
+        break;
+}
 
 include 'includes/footer.php';
