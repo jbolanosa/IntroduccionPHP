c78b203e2d7ac592e08de25f7cccebb4ef185229
José Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-04-01 11:03:05 -0600

    json

--- 18.json.php
+++ 18.json.php
@@ -1,6 +1,35 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'Tablet',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Television',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor',
+        'precio' => 400,
+        'disponible' => false
+    ],
+];
 
+
+echo "<pre>";
+var_dump($productos);
+
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE); //constante para agregar acentos
+
+$json_array = json_decode($json);
+
+
+var_dump($json);
+var_dump($json_array);
+echo "</pre>";
 
 
 
