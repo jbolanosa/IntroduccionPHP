a40f03d70a53285eb6bb3e8b6d4346ba8940752f
José Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-03-31 21:51:41 -0600

    foreach

--- 14-foreach.php
+++ 14-foreach.php
@@ -1,6 +1,35 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'Tablet',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Television',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor',
+        'precio' => 400,
+        'disponible' => false
+    ],
+];
 
+foreach( $productos as $producto){ ?>
+    <li>
+        <p>Producto : <?php echo $producto['nombre']; ?></p>
+        <p>Precio: <?php echo $producto['precio'];?></p>
+        <p><?php echo ($producto['disponible']) ? 'Disponible' : 'No Disponible'; ?></p>
+    </li>
+    <?php
+
+    // echo "<pre>";
+    // var_dump($producto);
+    // echo "</pre>";
+}
 
 
 include 'includes/footer.php';
